0440f426dbb18e9aa03e5231b6bdea2d0e54641b
Daz Oakley <do2@sanger.ac.uk>
2010-09-09 17:08:20 +0100

    Added in some tests for MartSearch::ServerUtils

---
+++ test/test_server_utils.rb
@@ -0,0 +1,18 @@
+require File.dirname(__FILE__) + '/test_helper.rb'
+
+class MartSearchServerUtilsTest < Test::Unit::TestCase
+  include MartSearch::ServerUtils
+  
+  def test_compressed_js
+    js = compressed_js()
+    assert( js.is_a?(String) )
+    assert( js.size > 0 )
+  end
+  
+  def test_compressed_css
+    css = compressed_css()
+    assert( css.is_a?(String) )
+    assert( css.size > 0 )
+  end
+  
+end
