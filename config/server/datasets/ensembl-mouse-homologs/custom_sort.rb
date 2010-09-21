98306f840e29e715d817e645d59c93701608f5af
Daz Oakley <do2@sanger.ac.uk>
2010-09-21 17:15:57 +0100

    Added in two new dataset configs...

---
+++ config/server/datasets/ensembl-mouse-homologs/custom_sort.rb
@@ -0,0 +1,30 @@
+sorted_results = {}
+
+results.each do |result|
+  joined_attribute = @config[:searching][:joined_attribute].to_sym
+  
+  if sorted_results[ result[ joined_attribute ] ].nil?
+    sorted_results[ result[ joined_attribute ] ] = {}
+    
+    result.keys.each do |key|
+      sorted_results[ result[ joined_attribute ] ][key] = []
+    end
+  end
+  
+  result_data = sorted_results[ result[ joined_attribute ] ]
+  
+  result.keys.each do |key|
+    result_data[key].push( result[key] )
+  end
+end
+
+# Finally, ensure that the data in the arrays is unique
+sorted_results.each do |key,result_data|
+  result_data.keys.each do |field|
+    if result_data[field].is_a?(Array)
+      result_data[field].uniq!
+    end
+  end
+end
+
+return sorted_results
---
+++ config/server/datasets/ikmc-omim/custom_sort.rb
@@ -0,0 +1,17 @@
+sorted_results = {}
+
+results.each do |result|
+  joined_attribute = @config[:searching][:joined_attribute].to_sym
+  
+  unless sorted_results[ result[ joined_attribute ] ]
+    sorted_results[ result[ joined_attribute ] ] = []
+  end
+  
+  sorted_results[ result[ joined_attribute ] ].push(result)
+end
+
+sorted_results.each do |key,omim_values|
+  sorted_results[key] = omim_values.sort{ |a,b| a['disorder_name'] <=> b['disorder_name'] }
+end
+
+return sorted_results
