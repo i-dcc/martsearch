e59119dfe93f5243d7b58317abb54bb76a75a948
Daz Oakley <do2@sanger.ac.uk>
2010-10-09 13:45:16 +0100

    Updated the test suite to match the new method calls

--- test/test_server_utils.rb
+++ test/test_server_utils.rb
@@ -4,13 +4,16 @@ class MartSearchServerUtilsTest < Test::Unit::TestCase
   include MartSearch::ServerUtils
   
   def test_compressed_js
-    js = compressed_js()
-    assert( js.is_a?(String) )
-    assert( js.size > 0 )
+    head_js = compressed_head_js( '0.0.1' )
+    assert( head_js.is_a?(String) )
+    assert( head_js.size > 0 )
+    base_js = compressed_base_js( '0.0.1' )
+    assert( base_js.is_a?(String) )
+    assert( base_js.size > 0 )
   end
   
   def test_compressed_css
-    css = compressed_css()
+    css = compressed_css( '0.0.1' )
     assert( css.is_a?(String) )
     assert( css.size > 0 )
   end
