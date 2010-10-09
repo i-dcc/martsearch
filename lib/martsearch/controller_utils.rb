f13412ec3b905d1eb0a028ecd4616c22c72c958e
Daz Oakley <do2@sanger.ac.uk>
2010-10-09 12:20:06 +0100

    Finished moving all datasets to the new js conf mech.

--- lib/martsearch/controller_utils.rb
+++ lib/martsearch/controller_utils.rb
@@ -116,7 +116,7 @@ module MartSearch
 
             dataview.stylesheet      = get_file_as_string("#{dv_location}/stylesheet.css")     if dv_conf[:custom_css]
             dataview.javascript_head = get_file_as_string("#{dv_location}/javascript_head.js") if dv_conf[:custom_head_js]
-            dataview.javascript_base = get_file_as_string("#{dv_location}/javascript.js")      if (dv_conf[:custom_js] or dv_conf[:custom_base_js])
+            dataview.javascript_base = get_file_as_string("#{dv_location}/javascript_base.js") if dv_conf[:custom_base_js]
 
             dataviews.push( dataview )
             dataviews_by_name[dv_name] = dataview
