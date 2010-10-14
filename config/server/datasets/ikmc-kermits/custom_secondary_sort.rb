a78d120f198fe10a23c38c7444f85b50fbaeb96c
Daz Oakley <do2@sanger.ac.uk>
2010-10-14 20:10:50 +0100

    Don't try to call .each() on nil objects...

--- config/server/datasets/ikmc-kermits/custom_secondary_sort.rb
+++ config/server/datasets/ikmc-kermits/custom_secondary_sort.rb
@@ -1,26 +1,28 @@
 
 search_data.each do |key,result_data|
-  if result_data[:'ikmc-kermits'] != nil and result_data[:'ikmc-idcc_targ_rep'] != nil
-    
-    # Cache the IKMC Project ID's for clones...
-    escell_cache = {}
-    result_data[:'ikmc-idcc_targ_rep'].each do |targ_rep_data|
-      [:conditional_clones,:nonconditional_clones].each do |clone_type|
+  next if result_data[:'ikmc-kermits'].nil? or result_data[:'ikmc-kermits'].empty?
+  next if result_data[:'ikmc-idcc_targ_rep'].nil? or result_data[:'ikmc-idcc_targ_rep'].empty?
+  
+  # Cache the IKMC Project ID's for clones...
+  escell_cache = {}
+  result_data[:'ikmc-idcc_targ_rep'].each do |targ_rep_data|
+    [:conditional_clones,:nonconditional_clones].each do |clone_type|
+      unless targ_rep_data[clone_type].nil?
         targ_rep_data[clone_type].each do |clone|
           escell_cache[ clone[:escell_clone] ] = clone[:ikmc_project_id]
         end
       end
     end
-    
-    # Now relate the mice to the cells/projects
-    mouse_data = []
-    result_data[:'ikmc-kermits'].each do |mouse|
-      mouse[:ikmc_project_id] = escell_cache[ mouse[:escell_clone] ]
-      mouse_data.push(mouse)
-    end
-    
-    result_data[:'ikmc-kermits'] = mouse_data
   end
+  
+  # Now relate the mice to the cells/projects
+  mouse_data = []
+  result_data[:'ikmc-kermits'].each do |mouse|
+    mouse[:ikmc_project_id] = escell_cache[ mouse[:escell_clone] ]
+    mouse_data.push(mouse)
+  end
+  
+  result_data[:'ikmc-kermits'] = mouse_data
 end
 
 return search_data
