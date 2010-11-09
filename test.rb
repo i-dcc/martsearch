7eaf12a1c59d2e7498be91e6ef153a52b94d46ca
Daz Oakley <do2@sanger.ac.uk>
2010-11-09 16:26:19 +0000

    File not required.

--- test.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-
-$:.unshift( "#{File.expand_path(File.dirname(__FILE__))}/lib" )
-require 'martsearch'
-
-builder = MartSearch::IndexBuilder.new()
-builder.fetch_datasets()
-builder.process_datasets()
