ed1aab87d931ac07341ebb15de80192e4ecb68ce
Daz Oakley <do2@sanger.ac.uk>
2010-11-10 22:04:33 +0000

    small test-suite change

--- test/test_server_utils.rb
+++ test/test_server_utils.rb
@@ -4,16 +4,16 @@ class MartSearchServerUtilsTest < Test::Unit::TestCase
   include MartSearch::ServerUtils
   
   def test_compressed_js
-    head_js = compressed_head_js( '0.0.1' )
+    head_js = compressed_head_js( 'test-suite' )
     assert( head_js.is_a?(String) )
     assert( head_js.size > 0 )
-    base_js = compressed_base_js( '0.0.1' )
+    base_js = compressed_base_js( 'test-suite' )
     assert( base_js.is_a?(String) )
     assert( base_js.size > 0 )
   end
   
   def test_compressed_css
-    css = compressed_css( '0.0.1' )
+    css = compressed_css( 'test-suite' )
     assert( css.is_a?(String) )
     assert( css.size > 0 )
   end
