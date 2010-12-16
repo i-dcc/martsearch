fd028b1e0bdcee9df128d6841b6cdac5202dca22
Daz Oakley <do2@sanger.ac.uk>
2010-12-16 11:55:54 +0000

    Version bump following JS and CSS changes.

--- lib/martsearch/server.rb
+++ lib/martsearch/server.rb
@@ -22,7 +22,7 @@ module MartSearch
     # We're going to use the version number as a cache breaker for the CSS 
     # and javascript code. Update with each release of your portal (especially 
     # if you change the CSS or JS)!!!
-    VERSION = '0.1.4'
+    VERSION = '0.1.5'
     DEFAULT_CSS_FILES = [
       'reset.css',
       'jquery.prettyPhoto.css',
