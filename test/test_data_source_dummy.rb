de7c9d211928029e38500a165cdef66f53d2fd7f
j1n3l0 <nelo.onyiah@gmail.com>
2011-03-03 15:48:53 +0000

    Update the tests ... add a stub test class for the Dummy data set [refs #4404]

--- test/test_data_source_dummy.rb
+++ test/test_data_source_dummy.rb
@@ -1,37 +1,26 @@
-require 'test_helper'
+require "test_helper"
 
 class MartSearchDummyDataSourceTest < Test::Unit::TestCase
   def setup
-    @conf_object      = MartSearch::Controller.instance()
+    @conf_object      = MartSearch::Controller.instance
     @dummy_datasource = MartSearch::DummyDataSource.new({})
   end
   
-  context 'A MartSearch::DummyDataSource object' do
-    should 'initialze correctly' do
+  context "A MartSearch::DummyDataSource object" do
+    should "initialze correctly" do
       assert( @dummy_datasource.is_a?(MartSearch::DummyDataSource) )
     end
     
-    should 'have a simple heartbeat function' do
+    should "have a simple heartbeat function" do
       assert( @dummy_datasource.is_alive? )
     end
     
-    should 'not have implemented fetch_all_terms_for_indexing()' do
+    should "not have implemented fetch_all_terms_for_indexing()" do
       assert_raise(NotImplementedError) { @dummy_datasource.fetch_all_terms_for_indexing( {} ) }
     end
-
-    # Test the return values for searches involving:
-    #
-    #               - no emma data
-    # Dync1h1       - no kermiks data
-    # Cbx1          - kermits and emma data
-    # B020004C17Rik - no kermits or emma data
-    should 'return the expeced data structure for search()' do
-    end
     
-    should 'not provide a URL link to the datasource of a search' do
-      assert_equal( nil, @dummy_datasource.data_origin_url( ['foo','bar'], {} ) )
+    should "not provide a URL link to the datasource of a search" do
+      assert_equal( nil, @dummy_datasource.data_origin_url( ["foo","bar"], {} ) )
     end
-    
   end
-  
 end
---
+++ test/test_dummy_data_set.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+# Test the return values for searches involving (NEEDS A BIT OF THOUGHT):
+#
+#               | no emma data            |             |
+# Dync1h1       | no kermiks data         |             |
+# Cbx1          | kermits and emma data   | MGI:105369  |
+# B020004C17Rik | no kermits or emma data | MGI:3588236 |
+#
+# Use MartSearchDataSetTest as a template.
+
+class MartSearchDummyDataSetTest < Test::Unit::TestCase
+end
