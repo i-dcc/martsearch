73af4f252c84687cbe3bfd2dd615201d03db41a2
j1n3l0 <nelo.onyiah@gmail.com>
2011-03-07 11:46:01 +0000

    We don't need the *ikmc-kermits* and *emma* dataviews anymore so remove the relevant files and update the configs [refs #4404]

--- config/server/dataviews/ikmc-mice/javascript_head.js
+++
@@ -1,8 +0,0 @@
-
-// ikmc-mice custom javascript
-
-jQuery("#search_results .ikmc-kermits_qc_details_toggle").live("click", function () {
-  jQuery(this).parent().parent().next("tr.ikmc-kermits_qc_details").toggle();
-  jQuery(this).toggleClass("toggle-open");
-  jQuery(this).toggleClass("toggle-close");
-});
---
+++ config/server/dataviews/mice/javascript_base.js
@@ -0,0 +1,8 @@
+
+// ikmc-mice custom javascript
+
+jQuery(document).ready(function() {
+  jQuery("#search_results .mice_qc_details").hide();
+  jQuery("#search_results .mice_qc_details_toggle").removeClass("toggle-open");
+  jQuery("#search_results .mice_qc_details_toggle").addClass("toggle-close");
+});
