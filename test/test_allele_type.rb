b3ca48c9c3be9f38cf95e751bd30c9c5c6d08e47
j1n3l0 <nelo.onyiah@gmail.com>
2011-03-10 16:16:10 +0000

    Add some more tests and simplify the structure [refs #4485]

--- test/test_allele_type.rb
+++ test/test_allele_type.rb
@@ -4,28 +4,32 @@ class TestMartSearchDataSetUtils < Test::Unit::TestCase
 
   include MartSearch::DataSetUtils
 
-  context "A valid allele_symbol" do
-    setup do
-      @allele_symbols = {
-        "tm1a(EUCOMM)Wtsi" => "Knockout-First",
-        "tm1e(EUCOMM)Wtsi" => "Targeted Non-Conditional",
-        "tm1(EUCOMM)Wtsi"  => "Deletion",
-      }
-    end
-
-    should "produce the correct allele_type" do
-      @allele_symbols.each do |allele_symbol, expected_type|
-        assert_equal expected_type, allele_type(allele_symbol), "not the expected type"
-      end
-    end
-  end
-
-  context "An invalid allele_symbol marked as a deletion" do
+  context "A valid allele_symbol and design_type" do
     setup do
       @example_data = [
         {
+          :expected_type => "Knockout-First",
+          :allele_symbol => "tm1a(EUCOMM)Wtsi",
+          :design_type   => nil,
+        },
+        {
+          :expected_type => "Targeted Non-Conditional",
+          :allele_symbol => "tm1e(EUCOMM)Wtsi",
+          :design_type   => nil,
+        },
+        {
+          :expected_type => "Deletion",
+          :allele_symbol => "tm1(EUCOMM)Wtsi",
+          :design_type   => nil,
+        },
+        {
           :expected_type => "Deletion",
           :allele_symbol => "Some Symbol",
+          :design_type   => "deletion",
+        },
+        {
+          :expected_type => "Deletion",
+          :allele_symbol => nil,
           :design_type   => "deletion",
         },
         {
@@ -34,21 +38,23 @@ class TestMartSearchDataSetUtils < Test::Unit::TestCase
           :design_type   => "Some Other Design Type",
         },
         {
+          :expected_type => "Knockout-First",
+          :allele_symbol => nil,
+          :design_type   => "Some Other Design Type",
+        },
+        {
           :expected_type => "",
           :allele_symbol => nil,
           :design_type   => nil,
-        }
+        },
       ]
     end
 
-    should "still produce the correct allele_type" do
+    should "produce the correct allele_type" do
       @example_data.each do |example|
         assert_equal example[:expected_type], allele_type(example[:allele_symbol], example[:design_type]),
         "Did not produce expected allele_type with #{example[:allele_symbol]} and #{example[:design_type]}"
       end
     end
   end
-
-  # what happens when the allele_symbol doesn't match any regex and
-  # design_type is nil?
 end
