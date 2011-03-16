801adfeb94e595fbc72d205686d51d7b6247fa40
Daz Oakley <do2@sanger.ac.uk>
2011-03-16 14:05:34 +0000

    Put a test for a DataSetUtils function in the test_data_set_utils suite!

--- test/test_data_set_utils.rb
+++ test/test_data_set_utils.rb
@@ -10,4 +10,54 @@ class MartSearchDataSetUtilsTest < Test::Unit::TestCase
     assert_equal( expt_str, fix_superscript_text_in_attribute(test_str) )
     assert_equal( expt_str, fix_superscript_text_in_attribute(expt_str) )
   end
+  
+  def test_allele_type
+    example_data = [
+      {
+        :expected_type => "Knockout-First",
+        :allele_symbol => "tm1a(EUCOMM)Wtsi",
+        :design_type   => nil,
+      },
+      {
+        :expected_type => "Targeted Non-Conditional",
+        :allele_symbol => "tm1e(EUCOMM)Wtsi",
+        :design_type   => nil,
+      },
+      {
+        :expected_type => "Deletion",
+        :allele_symbol => "tm1(EUCOMM)Wtsi",
+        :design_type   => nil,
+      },
+      {
+        :expected_type => "Deletion",
+        :allele_symbol => "Some Symbol",
+        :design_type   => "deletion",
+      },
+      {
+        :expected_type => "Deletion",
+        :allele_symbol => nil,
+        :design_type   => "deletion",
+      },
+      {
+        :expected_type => "Knockout-First",
+        :allele_symbol => "Some Symbol",
+        :design_type   => "Some Other Design Type",
+      },
+      {
+        :expected_type => "Knockout-First",
+        :allele_symbol => nil,
+        :design_type   => "Some Other Design Type",
+      },
+      {
+        :expected_type => "",
+        :allele_symbol => nil,
+        :design_type   => nil,
+      },
+    ]
+    
+    example_data.each do |example|
+      assert_equal example[:expected_type], allele_type(example[:allele_symbol], example[:design_type]),
+      "Did not produce expected allele_type with #{example[:allele_symbol]} and #{example[:design_type]}"
+    end
+  end
 end
