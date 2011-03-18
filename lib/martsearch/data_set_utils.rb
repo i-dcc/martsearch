5cd299ecd50186a626e0e7e47cdbd6237e251699
Daz Oakley <do2@sanger.ac.uk>
2011-03-18 15:16:38 +0000

    Move the allele_type function into the SeverViewHelpers.

--- lib/martsearch/data_set_utils.rb
+++ lib/martsearch/data_set_utils.rb
@@ -22,26 +22,5 @@ module MartSearch
       return attribute;
     end
     
-    # Helper function to retrieve the allele type
-    #
-    # @param  [String] allele_symbol The allele symbol superscript
-    # @param  [String] design_type   The design type
-    # @return [String]
-    def allele_type( allele_symbol, design_type=nil )
-       case allele_symbol
-       when /tm\d+a/ then "Knockout-First - Reporter Tagged Insertion"
-       when /tm\d+b/ then "Knockout-First, Post-Cre - Reporter Tagged Deletion"
-       when /tm\d+c/ then "Knockout-First, Post-Flp - Conditional"
-       when /tm\d+d/ then "Knockout-First, Post-Flp and Cre - Deletion, No Reporter"
-       when /tm\d+e/ then "Targeted Non-Conditional"
-       when /tm\d+\(/ then "Deletion"
-       else
-         case design_type
-         when nil          then ""
-         when /deletion/i  then "Deletion"
-         else                   "Knockout-First"
-         end
-       end
-    end
   end
 end
