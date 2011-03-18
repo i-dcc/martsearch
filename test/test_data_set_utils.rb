76db43cb0025895d56c692fc6f0172ae29d0fc9f
Daz Oakley <do2@sanger.ac.uk>
2011-03-18 13:50:51 +0000

    Flesh out the allele_type helper to cover more of the derivative alleles.

--- test/test_data_set_utils.rb
+++ test/test_data_set_utils.rb
@@ -10,4 +10,29 @@ class MartSearchDataSetUtilsTest < Test::Unit::TestCase
     assert_equal( expt_str, fix_superscript_text_in_attribute(test_str) )
     assert_equal( expt_str, fix_superscript_text_in_attribute(expt_str) )
   end
+  
+  def test_allele_type
+    assert_equal( "Knockout-First - Reporter Tagged Insertion", allele_type('tm1a(EUCOMM)WTSI') )
+    assert_equal( "Knockout-First - Reporter Tagged Insertion", allele_type('tm2a(EUCOMM)WTSI') )
+    assert_equal( "Knockout-First - Reporter Tagged Insertion", allele_type('tm10a(EUCOMM)WTSI') )
+    
+    assert_equal( "Knockout-First, Post-Cre - Reporter Tagged Deletion", allele_type('tm1b(EUCOMM)WTSI') )
+    assert_equal( "Knockout-First, Post-Cre - Reporter Tagged Deletion", allele_type('tm12b(EUCOMM)WTSI') )
+    
+    assert_equal( "Knockout-First, Post-Flp - Conditional", allele_type('tm1c(EUCOMM)WTSI') )
+    assert_equal( "Knockout-First, Post-Flp - Conditional", allele_type('tm12c(EUCOMM)WTSI') )
+    
+    assert_equal( "Knockout-First, Post-Flp and Cre - Deletion, No Reporter", allele_type('tm1d(EUCOMM)WTSI') )
+    assert_equal( "Knockout-First, Post-Flp and Cre - Deletion, No Reporter", allele_type('tm12d(EUCOMM)WTSI') )
+    
+    assert_equal( "Targeted Non-Conditional", allele_type('tm1e(EUCOMM)WTSI') )
+    assert_equal( "Targeted Non-Conditional", allele_type('tm12e(EUCOMM)WTSI') )
+    
+    assert_equal( "Deletion", allele_type('tm1(EUCOMM)WTSI') )
+    assert_equal( "Deletion", allele_type('tm12(EUCOMM)WTSI') )
+    
+    assert_equal( "", allele_type( nil, nil ) )
+    assert_equal( "Deletion", allele_type( nil, 'deletion' ) )
+    assert_equal( "Knockout-First", allele_type( nil, 'ko_first' ) )
+  end
 end
