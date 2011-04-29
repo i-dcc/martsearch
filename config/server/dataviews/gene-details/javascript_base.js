d0126f68ac3d87ca5ca2d6d20d3f99c18e002f4f
Daz Oakley <do2@sanger.ac.uk>
2011-04-29 13:02:47 +0100

    Small JS optimisation and cleanup...

--- config/server/dataviews/gene-details/javascript_base.js
+++ config/server/dataviews/gene-details/javascript_base.js
@@ -5,47 +5,36 @@ jQuery(".go_ontology_tree").each( function() {
   var id_arg = jQuery(this).attr('id');
 
   jQuery("#"+id_arg).jstree({
-    "core": {
-      "html_titles": true
+    core: {
+      html_titles: true
     },
-    "json_data": {
-      "ajax": {
-        "url": martsearch_url + "/go_ontology",
-        "data": function (n) {
+    json_data: {
+      ajax: {
+        url:  martsearch_url + "/go_ontology",
+        data: function (n) {
           return { id : n.attr ? n.attr("id") : id_arg };
         }
       }
     },
-    "themes": {
-      "theme": "classic",
-      "dots": true,
-      "icons": true
+    themes: {
+      theme: "classic",
+      dots:  true,
+      icons: true
     },
-    "types": {
-      "valid_children": ["default"],
-      "types": {
-        "default": {
-          "valid_children": [ "default", "leaf_node" ]
+    types: {
+      valid_children: ["default"],
+      types: {
+        "default":   {
+          valid_children: [ "default", "leaf_node" ]
         },
         "leaf_node": {
-          "icon": { "image": martsearch_url + "/images/silk/page_white.png" },
-          "valid_children": "none",
-          "hover_node": false
+          icon: { image: martsearch_url + "/images/silk/page_white.png" },
+          valid_children: "none",
+          hover_node: false
         }
       }
     },
-    "progressive_render": true,
-    "plugins": [ "json_data", "themes", "types" ]
+    progressive_render: true,
+    plugins: [ "json_data", "themes", "types" ]
   });
-  
-});
-
-jQuery("a.go_ontology_tree_open").live( "click", function() {
-  jQuery(this).parent().find(".go_ontology_tree").jstree("open_all");
-  return false;
-});
-
-jQuery("a.go_ontology_tree_close").live( "click", function() {
-  jQuery(this).parent().find(".go_ontology_tree").jstree("close_all");
-  return false;
 });
