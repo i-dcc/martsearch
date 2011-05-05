7ea3d0e503924636e0fbf0cfb10a56d813204915
Daz Oakley <do2@sanger.ac.uk>
2011-05-05 16:09:19 +0100

    More cleaning up of the param_level_heatmap data grab

--- config/server/datasets/wtsi-phenotyping-param_level_heatmap/custom_sort.rb
+++ config/server/datasets/wtsi-phenotyping-param_level_heatmap/custom_sort.rb
@@ -29,9 +29,6 @@ module MartSearch
         mp_groups   = sorted_results[joined_attribute][joined_attribute.to_sym][:mp_groups]
         test_groups = sorted_results[joined_attribute][joined_attribute.to_sym][:test_groups]
         
-        # Fix integers...
-        result[:parameter_order_by] = result[:parameter_order_by].to_i
-        
         # Now process and store the individual results data...
         wtsi_phenotyping_param_level_heatmap_sort_mp_heatmap_data( result, mp_groups )
         wtsi_phenotyping_param_level_heatmap_sort_test_group_data( result, test_groups )
@@ -96,8 +93,9 @@ module MartSearch
       protocol_description = result[:protocol_description]
       
       # Set up a storage object - remove lots of redundancy in the data
-      stored_result               = result.clone
-      fields_to_omit_from_storage = [
+      stored_result                      = result.clone
+      stored_result[:parameter_order_by] = stored_result[:parameter_order_by].to_i
+      fields_to_omit_from_storage        = [
         :pipeline, :colony_prefix, :param_level_heatmap_colony_prefix,
         :test, :protocol, :protocol_description, :mp_id, :mp_term
       ]
@@ -111,7 +109,7 @@ module MartSearch
       
       if protocol_description.nil?
         # This is a PDF (or collaborator) download - they don't have descriptions in the MIG system
-        test_groups[test] ||= { :significant => [], :insignificant => [] }
+        test_groups[test] ||= { :test => test, :significant => [], :insignificant => [] }
 
         significant = :insignificant
         significant = :significant if result[:manual_call] == 'Significant'
@@ -119,12 +117,10 @@ module MartSearch
         test_groups[test][significant].push(result)
       else
         # 'Regular' published graphs...
-        test_groups[test] ||= { :significant => {}, :insignificant => {} }
+        test_groups[test] ||= { :test => test, :significant => {}, :insignificant => {} }
         
         significant = :insignificant
         significant = :significant if result[:manual_call] == 'Significant'
-        
-        test_groups[test][significant][:test] = test
         
         # Use an MD5 hash of the test_description to correctly group the graphs - this 
         # helps sort out protocols that have the SAME name but DIFFERENT descriptions!
@@ -136,7 +132,7 @@ module MartSearch
         test_groups[test][significant][protocol_desc_hash][:protocol_description]   = protocol_description
         test_groups[test][significant][protocol_desc_hash][:pipeline]               = pipeline
         
-        graph_data = test_groups[test][significant][protocol_desc_hash][:graphs][result[:parameter_order_by]]
+        graph_data = test_groups[test][significant][protocol_desc_hash][:graphs][result[:parameter_order_by].to_sym]
         
         if graph_data.nil?
           graph_data = stored_result
@@ -144,7 +140,7 @@ module MartSearch
           graph_data[:mp_annotation].merge!(stored_result[:mp_annotation])
         end
         
-        test_groups[test][significant][protocol_desc_hash][:graphs][result[:parameter_order_by]] = graph_data
+        test_groups[test][significant][protocol_desc_hash][:graphs][result[:parameter_order_by].to_sym] = graph_data
       end
       
     end
