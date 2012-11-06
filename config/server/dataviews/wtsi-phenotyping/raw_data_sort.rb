3f287582e30df554a27a2dcbe18f4a6b2e8acc86
Richard Easty <re4@sanger.ac.uk>
2012-11-06 11:37:35 +0000

    detect nil object in raw_data_sort.rb

--- config/server/dataviews/wtsi-phenotyping/raw_data_sort.rb
+++ config/server/dataviews/wtsi-phenotyping/raw_data_sort.rb
@@ -1,15 +1,19 @@
 # encoding: utf-8
 
 class RawDataSort
-  
+
   #
   # Entry point for sorting phenotyping raw data by graph type.
   #
   # @param [Hash] the result set to sort.
   #
   def self.sort( results )
+    return nil, nil if ! results
+
     lresults    = self.strip_attribute_names( results )
     graph_type  = self.get_graph_type( lresults )
+
+    return nil, nil if ! graph_type
 
     case graph_type
     when :Bar
@@ -17,15 +21,15 @@ class RawDataSort
     else
       sorted_results = self.sort_data( lresults )
     end
-  
+
     return graph_type, sorted_results
   end
-  
-  
+
+
   private
 
   #
-  # Sort the data in the default way which is by gender and by membership (baseline, 
+  # Sort the data in the default way which is by gender and by membership (baseline,
   # control or subject).
   #
   # @param [Hash] the result set to sort.
@@ -40,7 +44,7 @@ class RawDataSort
       if !processed_results.has_key?(gender)
         processed_results[gender] = {}
       end
-      
+
       if !processed_results[gender].has_key?(membership)
         processed_results[gender][membership] = []
       end
@@ -48,23 +52,23 @@ class RawDataSort
     end
     return processed_results
   end
-  
-  
+
+
   #
   # Sorts the results for the bar graph type.
   #
   # @param [Hash] the result set to sort.
   #
   def self.sort_bar_data( results )
-  
-    processed_results = { 
+
+    processed_results = {
       :observations => [],
       :genders      => [],
-      :memberships  => [], 
+      :memberships  => [],
       :x_values     => [],
-      :results      => {} 
+      :results      => {}
     }
-  
+
     results.each do |result|
       observation = result[:observation]
       gender      = result[:gender]
@@ -83,14 +87,14 @@ class RawDataSort
         processed_results[:x_values].push( x_value )
       end
     end
-    
+
     results.each do |result|
       observation = result[:observation]
       membership  = self.get_membership( result )
       gender      = result[:gender]
       x_value     = result[:x_value]
       y_value     = result[:y_value]
-  
+
       if !processed_results[:results].has_key?( observation )
         processed_results[:results][observation] = {}
         processed_results[:genders].each do |g|
@@ -103,20 +107,20 @@ class RawDataSort
           end
         end
       end
-  
+
       processed_results[:results][observation][gender][membership][x_value] += 1
     end
-    
+
     processed_results[:memberships].sort!
     processed_results[:genders].sort!
     processed_results[:x_values].sort!
-    
+
     return processed_results
   end
-  
-  
+
+
   def self.sort_line_data( results )
-    
+
     processed_results = {
       :observations => [],
       :genders      => [],
@@ -124,13 +128,13 @@ class RawDataSort
       :time_points  => [],
       :results      => {}
     }
-    
+
     results.each do |result|
 
       gender      = result[:gender]
       observation = result[:observation]
       time_point  = result[:x_value]
-      
+
       if !processed_results[:genders].include?( gender )
         processed_results[:genders].push( gender )
       end
@@ -144,24 +148,24 @@ class RawDataSort
         processed_results[:time_points].push( time_point )
       end
     end
-    
+
     results.each do |result|
       observation = result[:observation]
       gender      = result[:gender]
-      
+
       if !processed_results[:results].has_key?( observation )
         processed_results[:results][observation] = {}
         if !processed_results[:results][observation].has_key?( gender )
           processed_results[:results][observation][gender] = {}
         end
       end
-      
+
     end
-    
+
   end
-  
+
   #
-  # Returns the string genotype for subject mice or the membership for mice in 
+  # Returns the string genotype for subject mice or the membership for mice in
   # the control or baseline.
   #
   # TODO: This can be simplified once the new :genotype_str column is added to the Biomart.
@@ -175,8 +179,8 @@ class RawDataSort
     end
     return result[:membership]
   end
-  
-  
+
+
   #
   # Strips the attribute name down to the basic. Removes the 'published_graph_data_' prefix
   # from each attribute key.
@@ -196,8 +200,8 @@ class RawDataSort
     end
     return processed_results
   end
-  
-  
+
+
   #
   # Returns the type of graph that the data is representing.
   #
@@ -205,6 +209,7 @@ class RawDataSort
   # @return [Symbol] The first results :graph_type key value.
   #
   def self.get_graph_type( results )
+    return nil if ! results || results.size == 0
     return results[0][:graph_type].to_sym
   end
 
