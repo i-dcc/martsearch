fdd046dd18fb61c38b2ad911ed0cd6ed851dd368
Peter Matthews <pm9@sanger.ac.uk>
2013-11-14 14:07:11 +0000

    remove reliance on ikmc solr index. Replaced with allele core solr index

--- lib/martsearch/project_utils.rb
+++ lib/martsearch/project_utils.rb
@@ -18,14 +18,46 @@ module MartSearch
     #
     # @param [String] project_id The IKMC project ID
     # @return [Hash] A hash containing all the data for the given project
+
+    def get_project_status_from_solr(project_id)
+      return nil if project_id.blank?
+      MartSearch::Controller.instance().logger.debug("[MartSearch::ProjectUtils] ::get_project_status_from_solr - running get_project_status_from_solr( '#{project_id}' )")
+
+      http_client = build_http_client()
+      url = "http://ikmc.vm.bytemark.co.uk:8983/solr/allele/select/?q=type%3Agene%0D%0Aproject_ids%3A#{project_id}&wt=json"
+#      url = "http://deskpro101887.internal.sanger.ac.uk:8983/solr/allele/select/?q=type%3Agene%0D%0Aproject_ids%3A#{project_id}&wt=json"
+      res = http_client.get( URI.parse(url) )
+      object = JSON.parse(res)
+
+      if object["response"]["numFound"] != 1
+        results = { :data => [ { :marker_symbol=>"", "mgi_accession_id"=>"", :ikmc_project=>"", :status=>""} ],
+                    :error => { :text  => "There was a problem querying the 'allele_core' Solr index. " + 'This supplies information on gene identifiers and IKMC tracking information. This page will not work without this datasource.' + " Try refreshing your browser or come back in 10 minutes.",
+                              :error => "Could not retreive data for project #{project_id}",
+                              :type  => ''
+                            }
+                  }
+      else
+        data = object["response"]["docs"][0]
+        results =  {
+            :data=> [ { :marker_symbol => data["marker_symbol"], :mgi_accession_id => data["mgi_accession_id"], :ikmc_project => data["ikmc_project_id"], :status => data["project_statuses"][data["project_ids"].find_index(project_id)] }],
+            :error=>{}
+          }
+      end
+
+      MartSearch::Controller.instance().logger.debug("[MartSearch::ProjectUtils] ::get_project_status_from_solr - running get_top_level_project_info( datasources, '#{project_id}' ) - DONE")
+
+      return results
+    end
+
     def get_ikmc_project_page_data( project_id )
-      MartSearch::Controller.instance().logger.debug("[MartSearch::ProjectUtils] ::get_ikmc_project_page_data - running get_ikmc_project_page_data( '#{project_id}' )")
+      MartSearch::Controller.instance().logger.debug("[MartSearch::ProjectUtils] ::get_ikmc_project_page_data - rget_project_status_from_solr( '#{project_id}' )")
       datasources = MartSearch::Controller.instance().datasources
       index       = MartSearch::Controller.instance().index
       data        = { :project_id => project_id.to_s }
       errors      = []
 
-      top_level_data = get_top_level_project_info( datasources, project_id )
+#      top_level_data = get_top_level_project_info( datasources, project_id )
+      top_level_data = get_project_status_from_solr( project_id )
 
       if top_level_data[:data].nil? or top_level_data[:data].empty?
         return { :data => nil }
@@ -143,7 +175,8 @@ module MartSearch
         ##
         ## Finally, categorize the stage of the pipeline that we are in
         ##
-
+        puts 'DATA STATUS'
+        puts data[:status]
         data.merge!( get_pipeline_stage( data[:status]) ) if data[:status]
       end
 
